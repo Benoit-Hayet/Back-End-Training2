cbf906daa7d72aad0c78b524b8b11a7227646ce8
Benoit-Hayet <hayetbenoit@gmail.com>
2024-01-29 21:55:54 +0100

    add manager

---
+++ backend/models/MovieManager.js
@@ -0,0 +1,15 @@
+const AbstractManager = require("./AbstractManager");
+
+class MovieManager extends AbstractManager  {
+    constructor() {
+      super({ table: "movie" });
+    }
+    
+    getMovies = (id,title,director,release_year,genre,image_url) => {
+        return this.database
+        const sqlQuery =
+       `select id,title,director,release_year,genre,image_url
+       `;
+    
+}
+    module.exports = MovieManager;
