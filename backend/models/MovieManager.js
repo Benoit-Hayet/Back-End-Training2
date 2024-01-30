ee00b938dde50463af46a01281a271af131e7d8a
Benoit-Hayet <hayetbenoit@gmail.com>
2024-01-30 21:14:01 +0100

    Add router open to work

--- backend/models/MovieManager.js
+++ backend/models/MovieManager.js
@@ -4,6 +4,7 @@ class MovieManager extends AbstractManager  {
     constructor() {
       super({ table: "movie" });
     }
+  }
     
     getMovies = (id,title,director,release_year,genre,image_url) => {
         return this.database
